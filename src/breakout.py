8b70341beb454ec29f587f4487aa007d3bbddd52
yuduki <rtccb.yuduki24@gmail.com>
2017-11-04 12:24:30 +0900

    [add]ブロックを壊す機能の追加.

--- src/breakout.py
+++ src/breakout.py
@@ -11,14 +11,21 @@ def main():
     pygame.display.set_caption(u"ブロック崩し")
     
     # スプライトグループを作成して登録
-    all = pygame.sprite.RenderUpdates()
+    all = pygame.sprite.RenderUpdates()  # 描画用グループ
+    blocks = pygame.sprite.Group()       # 衝突判定用グループ
     Paddle.containers = all
     Ball.containers = all
+    Block.containers = all, blocks
 
     # パドルを作成
     paddle = Paddle()
     # ボールを作成するとスプライトグループallに自動的に追加される
-    Ball(paddle)
+    Ball(paddle, blocks)
+    # ブロックを作成
+    # 自動的にblocksグループに追加される
+    for x in range(1, 11):  # 1列から10列まで
+        for y in range(1, 6):  # 1行から5行まで
+            Block(x, y)
 
     clock = pygame.time.Clock()
     while True:
@@ -49,11 +56,12 @@ class Paddle(pygame.sprite.Sprite):
 class Ball(pygame.sprite.Sprite):
     """ボール"""
     speed = 5
-    def __init__(self, paddle):
+    def __init__(self, paddle, blocks):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.image, self.rect = load_image("ball.png")
         self.dx = self.dy = 0  # ボールの速度
         self.paddle = paddle  # パドルへの参照
+        self.blocks = blocks  # blocksへの参照.
         self.update = self.start
     def start(self):
         """ボールの位置を初期化"""
@@ -86,6 +94,37 @@ class Ball(pygame.sprite.Sprite):
         # ボールを落とした場合
         if self.rect.top > SCR_RECT.bottom:
             self.update = self.start  # ボールを初期状態に
+
+        # ブロックを壊す
+        # ボールと衝突したブロックリストを取得
+        blocks_collided = pygame.sprite.spritecollide(self, self.blocks, True)
+        if blocks_collided:  # 衝突ブロックがある場合
+            oldrect = self.rect
+            for brick in blocks_collided:  # 各衝突ブロックに対して
+                # ボールが左から衝突
+                if oldrect.left < brick.rect.left < oldrect.right < brick.rect.right:
+                    self.rect.right = brick.rect.left
+                    self.dx = -self.dx
+                # ボールが右から衝突
+                if brick.rect.left < oldrect.left < brick.rect.right < oldrect.right:
+                    self.rect.left = brick.rect.right
+                    self.dx = -self.dx
+                # ボールが上から衝突
+                if oldrect.top < brick.rect.top < oldrect.bottom < brick.rect.bottom:
+                    self.rect.bottom = brick.rect.top
+                    self.dy = -self.dy
+                # ボールが下から衝突
+                if brick.rect.top < oldrect.top < brick.rect.bottom < oldrect.bottom:
+                    self.rect.top = brick.rect.bottom
+                    self.dy = -self.dy
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image, self.rect = load_image("brick.png")
+        # ブロックの位置を更新
+        self.rect.left = SCR_RECT.left + x * self.rect.width
+        self.rect.top = SCR_RECT.top + y * self.rect.height
 
 def load_image(filename, colorkey=None):
     """画像をロードして画像と矩形を返す"""
